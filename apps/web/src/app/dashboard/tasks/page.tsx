48320fc2411e8ad5ed5a387a6e6cd97df5ab3f7e
unknown <andikapiyo12@gmail.com>
2026-01-18 15:53:31 +0700

    update cursor

--- apps/web/src/app/dashboard/tasks/page.tsx
+++ apps/web/src/app/dashboard/tasks/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 export default function Tasks() {
     return (
-        <div>
+        <div className='ml-20'>
             <h1>This is Tasks Page!</h1>
         </div>
     )
